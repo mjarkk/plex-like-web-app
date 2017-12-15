22c6a99cd6cdbd39687d81a96efb99360004140f
Mark Kopenga <mkopenga@gmail.com>
2017-12-15 08:49:46 +0100

    Completed login screen

--- dev/js/login.js
+++ dev/js/login.js
@@ -3,19 +3,43 @@ window.LoadedScripts['login'] = true
 var login = new Vue({
   el: '.login-vue',
   data: {
-    showbgimg: false
+    showbgimg: false,
+    showloginprocess: false,
+    username: '',
+    password: ''
   },
   methods: {
     trylogin: () => {
-      log('login thingy fired')
+      let vm = this
+      fetch('/getsalt', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Cache': 'no-cache'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          username: vm.username
+        })
+      })
+      .then((res) => res.json())
+      .then((jsondata) => {
+        log(jsondata)
+      })
+      .catch((err) => {
+        vm.showloginprocess = true
+      })
     }
   },
   created: () => {
-    let imgloader = new Image(window.innerHeight, window.innerWidth)
-    imgloader.onload = () => {
-      document.querySelector('.login-vue').style.backgroundImage = `url(${imgloader.src})`
-      login.showbgimg = true
-    }
-    imgloader.src = `https://source.unsplash.com/collection/791207/${Math.round(window.innerWidth * 1.3)}x${Math.round(window.innerHeight * 1.3)}`
+
   }
 })
+
+let imgloader = new Image()
+imgloader.src = `https://source.unsplash.com/collection/791207/${Math.round(window.innerWidth * 1.3)}x${Math.round(window.innerHeight * 1.3)}`
+imgloader.onload = () => {
+  document.querySelector('.login-vue').style.backgroundImage = `url(${imgloader.src})`
+  login.showbgimg = true
+}
